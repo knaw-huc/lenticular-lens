3344d25d3bfde5c79502a7951813189bc1f6a941
Cedric van Bommel <cedric.vanbommel@ogd.nl>
2019-01-30 09:51:46 +0100

    Fix catching exceptions in threaded process and improve retrying

--- timbuctoo_worker/src/timbuctoo_worker.py
+++ timbuctoo_worker/src/timbuctoo_worker.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 from config_db import db_conn, run_query
 from datasets_config import DatasetsConfig
 import datetime
@@ -5,21 +6,39 @@ from psycopg2 import extras as psycopg2_extras
 import psycopg2
 import random
 import signal
-import threading
 import time
+
+
+def reset_job():
+    if job:
+        print('Trying to reset job...')
+
+        rt = 0
+        while True:
+            try:
+                with db_conn() as conn:
+                    with conn.cursor(cursor_factory=psycopg2_extras.DictCursor) as cur:
+                        cur.execute(
+                            "UPDATE timbuctoo_jobs SET processing_at = NULL WHERE id = %s",
+                            (job['id'], )
+                        )
+                    conn.commit()
+
+            except (psycopg2.InterfaceError, psycopg2.OperationalError):
+                rt += 1
+                print('Database error. Waiting...')
+                time.sleep(0.5 + (random.randint(0, 1000) / 1000))
+                print('Retry %i...' % rt)
+                continue
+            else:
+                print('Job reset.')
+                return
 
 
 def teardown(signum=0, frame=None):
     print('Stopping Timbuctoo worker.')
 
-    if job:
-        with db_conn() as conn:
-            with conn.cursor(cursor_factory=psycopg2_extras.DictCursor) as cur:
-                cur.execute(
-                    "UPDATE timbuctoo_jobs SET processing_at = NULL WHERE id = %s",
-                    (job['id'], )
-                )
-            conn.commit()
+    reset_job()
 
     exit(signum)
 
@@ -27,56 +46,65 @@ def teardown(signum=0, frame=None):
 if __name__ == '__main__':
     signal.signal(signal.SIGTERM, teardown)
 
+    job = None
+    n1 = 0
+
     while True:
-        job = None
-        n1 = 0
         try:
             with db_conn() as conn:
-                while True:
-                    print('Looking for new Timbuctoo job...')
+                print('Looking for new Timbuctoo job...')
 
-                    while not job:
-                        with conn.cursor(cursor_factory=psycopg2_extras.DictCursor) as cur:
-                            cur.execute("LOCK TABLE timbuctoo_jobs IN ACCESS EXCLUSIVE MODE;")
-                            cur.execute("""
-                                SELECT *
-                                FROM timbuctoo_jobs
-                                WHERE processing_at IS NULL
-                                AND finished_at IS NULL
-                                ORDER BY requested_at
-                                LIMIT 1;""")
-
-                            job = cur.fetchone()
-
-                            if not job:
-                                conn.commit()
-                                time.sleep(2)
-
-                    print('Job %s started.' % job['id'])
-
-                    process_start_time = str(datetime.datetime.now())
+                # By not setting job = None here, job will be retried after exception
+                while not job:
                     with conn.cursor(cursor_factory=psycopg2_extras.DictCursor) as cur:
-                        cur.execute(
-                            "UPDATE timbuctoo_jobs SET processing_at = %s WHERE id = %s",
-                            (process_start_time, job['id'])
-                        )
-                    conn.commit()
+                        cur.execute("LOCK TABLE timbuctoo_jobs IN ACCESS EXCLUSIVE MODE;")
+                        cur.execute("""
+                            SELECT *
+                            FROM timbuctoo_jobs
+                            WHERE processing_at IS NULL
+                            AND finished_at IS NULL
+                            ORDER BY requested_at
+                            LIMIT 1;""")
 
-                    collection = DatasetsConfig().dataset(job['dataset_id']).collection(job['collection_id'])
-                    thread = threading.Thread(target=collection.create_cached_view, args=(job['limit'], False))
-                    thread.start()
-                    thread.join()
+                        job = cur.fetchone()
+                        n1 = 0
 
-                    process_finish_time = str(datetime.datetime.now())
-                    run_query(
-                        "UPDATE timbuctoo_jobs SET finished_at = %s WHERE id = %s",
-                        (process_finish_time, job['id'])
+                        if not job:
+                            conn.commit()
+                            time.sleep(2)
+
+                print('Job %s started.' % job['id'])
+
+                process_start_time = str(datetime.datetime.now())
+                with conn.cursor(cursor_factory=psycopg2_extras.DictCursor) as cur:
+                    cur.execute(
+                        "UPDATE timbuctoo_jobs SET processing_at = %s WHERE id = %s",
+                        (process_start_time, job['id'])
                     )
+                conn.commit()
+                n1 = 0
 
-                    print('Job %s finished.' % job['id'])
+                collection = DatasetsConfig().dataset(job['dataset_id']).collection(job['collection_id'])
+                with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+                    future = [executor.submit(collection.create_cached_view, job['limit'], False)]
+                    for completed_future in concurrent.futures.as_completed(future):
+                        completed_future.result()
 
-                    job = None
+                process_finish_time = str(datetime.datetime.now())
+                run_query(
+                    "UPDATE timbuctoo_jobs SET finished_at = %s WHERE id = %s",
+                    (process_finish_time, job['id'])
+                )
+
+                print('Job %s finished.' % job['id'])
+
+                job = None
+                n1 = 0
 
         except (psycopg2.InterfaceError, psycopg2.OperationalError):
             n1 += 1
+            print('Database error. Waiting...')
             time.sleep((2 ** n1) + (random.randint(0, 1000) / 1000))
+            print('Retry %i...' % n1)
+
+            continue
