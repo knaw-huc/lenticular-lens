72909016fc722fbeeab56b9e8935c786b6c10013
Cedric van Bommel <cedric.vanbommel@ogd.nl>
2019-01-31 15:54:51 +0100

    Update Timbuctoo worker for use with timbuctoo_tables without real data fetching

--- timbuctoo_worker/src/timbuctoo_worker.py
+++ timbuctoo_worker/src/timbuctoo_worker.py
@@ -1,52 +1,11 @@
-import concurrent.futures
 from config_db import db_conn, run_query
-from datasets_config import DatasetsConfig
-import datetime
 from psycopg2 import extras as psycopg2_extras
 import psycopg2
 import random
-import signal
 import time
 
 
-def reset_job():
-    if job:
-        print('Trying to reset job...')
-
-        rt = 0
-        while True:
-            try:
-                with db_conn() as conn:
-                    with conn.cursor(cursor_factory=psycopg2_extras.DictCursor) as cur:
-                        cur.execute(
-                            "UPDATE timbuctoo_jobs SET processing_at = NULL WHERE id = %s",
-                            (job['id'], )
-                        )
-                    conn.commit()
-
-            except (psycopg2.InterfaceError, psycopg2.OperationalError):
-                rt += 1
-                print('Database error. Waiting...')
-                time.sleep(0.5 + (random.randint(0, 1000) / 1000))
-                print('Retry %i...' % rt)
-                continue
-            else:
-                print('Job reset.')
-                return
-
-
-def teardown(signum=0, frame=None):
-    print('Stopping Timbuctoo worker.')
-
-    reset_job()
-
-    exit(signum)
-
-
 if __name__ == '__main__':
-    signal.signal(signal.SIGTERM, teardown)
-
-    job = None
     n1 = 0
 
     while True:
@@ -54,16 +13,20 @@ if __name__ == '__main__':
             with db_conn() as conn:
                 print('Looking for new Timbuctoo job...')
 
-                # By not setting job = None here, job will be retried after exception
+                job = None
                 while not job:
                     with conn.cursor(cursor_factory=psycopg2_extras.DictCursor) as cur:
-                        cur.execute("LOCK TABLE timbuctoo_jobs IN ACCESS EXCLUSIVE MODE;")
+                        cur.execute("LOCK TABLE timbuctoo_tables IN ACCESS EXCLUSIVE MODE;")
                         cur.execute("""
                             SELECT *
-                            FROM timbuctoo_jobs
-                            WHERE processing_at IS NULL
-                            AND finished_at IS NULL
-                            ORDER BY requested_at
+                            FROM timbuctoo_tables
+                            WHERE update_start_time IS NULL
+                            OR (
+                                (update_finish_time IS NULL OR update_finish_time < update_start_time)
+                                AND update_start_time < now() - interval '1 minute'
+                                AND (last_push_time IS NULL OR last_push_time < now() - interval '1 minute')
+                            )
+                            ORDER BY create_time
                             LIMIT 1;""")
 
                         job = cur.fetchone()
@@ -73,30 +36,35 @@ if __name__ == '__main__':
                             conn.commit()
                             time.sleep(2)
 
-                print('Job %s started.' % job['id'])
+                print('Job for table %s started.' % job['table_name'])
 
-                process_start_time = str(datetime.datetime.now())
-                with conn.cursor(cursor_factory=psycopg2_extras.DictCursor) as cur:
+                with conn.cursor() as cur:
                     cur.execute(
-                        "UPDATE timbuctoo_jobs SET processing_at = %s WHERE id = %s",
-                        (process_start_time, job['id'])
+                        'UPDATE timbuctoo_tables SET update_start_time = now() WHERE "table_name" = %s',
+                        (job['table_name'],)
                     )
                 conn.commit()
                 n1 = 0
 
-                collection = DatasetsConfig().dataset(job['dataset_id']).collection(job['collection_id'])
-                with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-                    future = [executor.submit(collection.create_cached_view, job['limit'], False)]
-                    for completed_future in concurrent.futures.as_completed(future):
-                        completed_future.result()
+                for i in range(5):
+                    print("Pretending I'm doing my job...")
+                    time.sleep(3)
+                    rows_inserted = 100
 
-                process_finish_time = str(datetime.datetime.now())
+                    with conn.cursor() as cur:
+                        cur.execute(
+                            'UPDATE timbuctoo_tables SET last_push_time = now(), rows_count = rows_count + %s WHERE "table_name" = %s',
+                            (rows_inserted, job['table_name'])
+                        )
+
+                    conn.commit()
+
                 run_query(
-                    "UPDATE timbuctoo_jobs SET finished_at = %s WHERE id = %s",
-                    (process_finish_time, job['id'])
+                    'UPDATE timbuctoo_tables SET update_finish_time = now() WHERE "table_name" = %s',
+                    (job['table_name'],)
                 )
 
-                print('Job %s finished.' % job['id'])
+                print('Job for table %s finished.' % job['table_name'])
 
                 job = None
                 n1 = 0
