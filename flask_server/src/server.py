8ea209e39478f052f9960d10796c5f8e539d2f4a
Cedric van Bommel <cedric.vanbommel@ogd.nl>
2019-02-15 15:56:55 +0100

    Add route '/datasets'

--- flask_server/src/server.py
+++ flask_server/src/server.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from datasets_config import DatasetsConfig
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
+
+@app.route('/datasets')
+def datasets():
+    return jsonify(DatasetsConfig().data)
