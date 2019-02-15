72f500462dd883c3c1064ebe8ae3f26cd2855eb1
Cedric van Bommel <cedric.vanbommel@ogd.nl>
2019-02-15 16:15:28 +0100

    Add POST route '/handle_json_upload/'

--- flask_server/src/server.py
+++ flask_server/src/server.py
@@ -1,5 +1,8 @@
 from datasets_config import DatasetsConfig
-from flask import Flask, jsonify
+import datetime
+from flask import Flask, jsonify, request
+from helpers import get_job_data, hash_string, update_job_data
+import json
 app = Flask(__name__)
 
 
@@ -11,3 +14,38 @@ def index():
 @app.route('/datasets')
 def datasets():
     return jsonify(DatasetsConfig().data)
+
+
+@app.route('/handle_json_upload/', methods=['POST'])
+def handle_json_upload():
+    resources_json = json.dumps(request.json['resources'], indent=2)
+    resources_filename = './generated_json/resources_' + hash_string(resources_json) + '.json'
+
+    matches_json = json.dumps(request.json['matches'], indent=2)
+    matches_filename = './generated_json/matches_' + hash_string(matches_json) + '.json'
+
+    job_id = hash_string(resources_filename.split('/')[-1] + matches_filename.split('/')[-1])
+
+    response = {'job_id': job_id}
+
+    if not get_job_data(job_id):
+        json_file = open(resources_filename, 'w')
+        json_file.write(resources_json)
+        json_file.close()
+
+        json_file = open(matches_filename, 'w')
+        json_file.write(matches_json)
+        json_file.close()
+
+        job_data = {
+            'resources_form_data': json.dumps(request.json['resources_original']),
+            'mappings_form_data': json.dumps(request.json['matches_original']),
+            'resources_filename': resources_filename,
+            'mappings_filename': matches_filename,
+            'status': 'Requested',
+            'requested_at': datetime.datetime.now(),
+        }
+
+        update_job_data(job_id, job_data)
+
+        return jsonify(response)
