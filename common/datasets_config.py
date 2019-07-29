e3bde37662e1d8c548740cb4fa32ac7af96917d0
kerim1 <kerim1@users.noreply.github.com>
2019-07-29 16:59:34 +0200

    Add datasets from database as fallback

--- common/datasets_config.py
+++ common/datasets_config.py
@@ -10,7 +10,19 @@ class DatasetsConfig:
         n = 0
         while True:
             try:
-                self.__data = Timbuctoo().datasets
+                timbuctoo_data = Timbuctoo().datasets
+                database_data = Dataset.datasets()
+
+                combined = timbuctoo_data.copy()
+                for dataset, dataset_data in database_data.items():
+                    if not dataset in combined:
+                        combined[dataset] = dataset_data.copy()
+                    else:
+                        for collection, dataset_data in dataset_data['collections'].items():
+                            if not collection in combined[dataset]['collections']:
+                                combined[dataset]['collections'][collection] = database_data.copy()
+
+                self.__data = combined
             except (psycopg2.InterfaceError, psycopg2.OperationalError):
                 n += 1
                 print('Database error. Retry %i' % n)
