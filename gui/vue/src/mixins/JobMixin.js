b9a387fd9dcb392ceb65ccb95fcabb5e7f1b44de
kerim1 <kerim1@users.noreply.github.com>
2021-01-19 10:04:22 +0100

    Add target transformer updates too

--- gui/vue/src/mixins/JobMixin.js
+++ gui/vue/src/mixins/JobMixin.js
@@ -240,6 +240,13 @@ export default {
                             method.targets.forEach(target => {
                                 target.transformers = target.transformers.filter(transformer =>
                                     transformer.name !== 'PARSE_DATE' && transformer.name !== 'PARSE_NUMERIC');
+
+                                target.transformers.forEach(transformer => {
+                                    if (transformer.name === 'ECARTICO_FULL_NAME') {
+                                        transformer.name = 'TRANSFORM_LAST_NAME_FORMAT';
+                                        transformer.parameters = {infix: true};
+                                    }
+                                });
                             });
                         }
                     });
